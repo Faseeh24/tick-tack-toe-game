93da2e953f5d6352529df7071fbe6759d098cab6
Muhammad Faseeh Ur Rehman <139424171+Faseeh24@users.noreply.github.com>
2023-10-03 20:53:16 +0500

    Added game basic layout code

---
+++ Tick Tack Toe Game.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    cout << "\tT i c k   T a c k   T o e\n" << endl;
+    cout << "Player1 [X]" << endl
+        << "Player2 [O]\n\n"<<endl;
+    
+    cout << "\t         |         |         " << endl;
+    cout << "\t    1    |    2    |    3    " << endl;
+    cout << "\t         |         |         " << endl;
+    cout << "\t---------|---------|---------" << endl;
+    cout << "\t         |         |         " << endl;
+    cout << "\t    4    |    5    |    6    " << endl;
+    cout << "\t         |         |         " << endl;
+    cout << "\t---------|---------|---------" << endl;
+    cout << "\t         |         |         " << endl;
+    cout << "\t    7    |    8    |    9    " << endl;
+    cout << "\t         |         |         " << endl;
+        
+    
+    cout << "Player1 [X] turn: " << endl;
+    
+    
+    return 0;
+}
+    
